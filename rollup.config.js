7ab280f14d2594201165447460dd66ce123aa219
Jason Desrosiers <jdesrosi@gmail.com>
2019-07-14 22:24:01 -0700

    Initial Implementation

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import resolve from "rollup-plugin-node-resolve";
 import replace from "rollup-plugin-replace";
 import commonjs from "rollup-plugin-commonjs";
 import svelte from "rollup-plugin-svelte";
-import babel from "rollup-plugin-babel";
 import { terser } from "rollup-plugin-terser";
 import config from "sapper/config/rollup";
 import pkg from "./package.json";
@@ -10,7 +9,6 @@ import pkg from "./package.json";
 
 const mode = process.env.NODE_ENV;
 const dev = mode === "development";
-const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) => (warning.code === "CIRCULAR_DEPENDENCY" && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
 const dedupe = (importee) => importee === "svelte" || importee.startsWith("svelte/");
@@ -34,23 +32,6 @@ export default {
         dedupe
       }),
       commonjs(),
-
-      legacy && babel({
-        extensions: [".js", ".mjs", ".html", ".svelte"],
-        runtimeHelpers: true,
-        exclude: ["node_modules/@babel/**"],
-        presets: [
-          ["@babel/preset-env", {
-            targets: "> 0.25%, not dead"
-          }]
-        ],
-        plugins: [
-          "@babel/plugin-syntax-dynamic-import",
-          ["@babel/plugin-transform-runtime", {
-            useESModules: true
-          }]
-        ]
-      }),
 
       !dev && terser({
         module: true
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,109 @@
+<script>
+  import Hyperjump from "@hyperjump/browser";
+  import Validation from "@hyperjump/validation";
+
+
+  let validation = "";
+  const validationUrl = "https://mock.hyperjump.io/validation";
+
+  $: validate = (async function () {
+    if (validation !== "") {
+      fetchStub.set(validationUrl, validation, "application/reference+json");
+
+      const doc = Hyperjump.get(validationUrl, Hyperjump.nil);
+      return Validation.validate(doc);
+    }
+  }());
+
+  let subject = "";
+  const subjectUrl = "https://mock.hyperjump.io/subject";
+
+  $: result = (async function () {
+    if (subject !== "") {
+      fetchStub.set(subjectUrl, subject, "application/reference+json");
+
+      const doc = Hyperjump.get(subjectUrl, Hyperjump.nil);
+
+      let v;
+      try {
+        v = await validate;
+      } catch (e) {}
+
+      if (v) {
+        return v(doc);
+      }
+    }
+  }());
+</script>
+
 <svelte:head>
 	<title>Hyperjump Validation</title>
 </svelte:head>
 
-<h1>Hyperjump Validation</h1>
+<main>
+  <h1>Hyperjump Validation</h1>
+  <div class="foo">
+    <div class="bar">
+      <h2>Validation</h2>
+      <textarea name="validation" bind:value={validation}></textarea>
+      <pre class="results">
+        {#await validate then v}
+          {#if v}
+            Valid
+          {/if}
+        {:catch error}
+          {Array.isArray(error) ? JSON.stringify(error, null, "  ") : error}
+        {/await}
+      </pre>
+    </div>
+    <div class="bar">
+      <h2>Subject</h2>
+      <textarea name="subject" bind:value={subject}></textarea>
+      <pre class="results">
+        {#await result then r}
+          {#if r}
+            {Validation.isValid(r) ? "Valid" : JSON.stringify(r, null, "  ")}
+          {/if}
+        {:catch error}
+          {error}
+        {/await}
+      </pre>
+    </div>
+  </div>
+</main>
+
+<style>
+  .foo {
+    display: flex;
+    height: 100%;
+    width: 100%;
+  }
+
+  .bar {
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    padding: 1em;
+  }
+
+  .results {
+    border: thin solid black;
+    padding: .5em;
+    min-height: 100px;
+    overflow: scroll;
+  }
+
+  h1 {
+    margin: 1em auto;
+  }
+
+  textarea {
+    display: block;
+    height: 100%;
+    border: thin solid black;
+    min-height: 300px;
+    font-size: 16px;
+    padding: .5em;
+    margin-bottom: 1em;
+  }
+</style>
